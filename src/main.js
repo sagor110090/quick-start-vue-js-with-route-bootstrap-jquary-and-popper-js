44b3d213f1cb365f6e7b3267e64a26b98e0c59c1
sagor110090 <hasan15-4933@diu.edu.bd>
2020-01-23 12:53:36 +0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import jQuery from 'jquery'
+window.$ = window.jQuery = jQuery
+
+import 'popper.js'
+import 'bootstrap'
+import './assets/app.scss'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
